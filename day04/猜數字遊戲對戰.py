3cf61f39957433df356c04304ff56e1cf262f49c
Lawrence Wang <rustywang0305@gmail.com>
2021-05-05 20:49:30 +0800

    python

--- day04/猜數字遊戲對戰.py
+++ day04/猜數字遊戲對戰.py
@@ -4,16 +4,45 @@ min = 0
 max = 100
 count = 5
 while count > 0:
+    # 使用者猜
     guess = int(input('(%d). 請輸入 %d ~ %d :' % (count, min, max)))
     if guess >= max or guess <= min: # 檢查輸入數值
         print('請重新輸入')
         continue
     # 將 count 減去一次
     count = count - 1
+    # 判定結果
     if guess > ans:
         max = guess
     elif guess < ans:
         min = guess
     else:
-        print('答對了')
+        print('使用者答對了')
         break
+
+    # 電腦猜
+    guess = r.randint(min + 1, max - 1)
+    print('(%d). 電腦猜 %d ~ %d : %d' % (count, min, max, guess))
+    # 判定結果
+    if guess > ans:
+        max = guess
+    elif guess < ans:
+        min = guess
+    else:
+        print('電腦答對了')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
